b7d269e8ff39579b72fb0858a1b87ebc6e79d439
sufuan <abu49539@gmail.com>
2022-03-01 22:25:50 +0600

    done

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const errMsg = document.getElementById('error-message');
+const spinner = document.getElementById('spinner')
 
+spinner.style.display = 'none'
 const loadData = () => {
+        document.getElementById('display-card').innerHTML = ''
+        document.getElementById('product-photo').innerHTML = ''
+        document.getElementById('phone-details-mainfeature').innerHTML = ''
+        document.getElementById('phone-details-others').innerHTML = ''
         const searchInput = document.getElementById('search-input')
-
+        spinner.style.display = 'block';
         if (!isNaN(searchInput.value)) {
             errMsg.innerText = 'Enter valid input'
+            spinner.style.display = 'none';
         } else {
 
             errMsg.innerText = ''
@@ -17,16 +24,25 @@ const loadData = () => {
                 .then(res => res.json())
                 .then(data => {
                     if (data.data === null || data.data.length === 0) {
-                        console.log('objection')
+
+                        setTimeout(spinnerTimeoutMessage, 3000);
+
                     } else {
 
                         displayData(data.data);
+                        spinner.style.display = 'none';
                     }
                 })
 
 
         }
     } // loadData end
+
+
+const spinnerTimeoutMessage = () => {
+    spinner.style.display = 'none';
+    const errMsg = document.getElementById('error-message').innerText = 'Search result not found';
+}
 
 //display loaded data
 
@@ -37,25 +53,33 @@ const displayData = (allDatas) => {
     document.getElementById('phone-details-others').innerHTML = ''
     const allData = allDatas.slice(0, 20)
     allData.forEach(data => {
+
+
         const div = document.createElement('div');
         div.classList.add('col-lg-4', 'p-2');
 
+
         div.innerHTML = `
+
         <div class="card" style="width: 18rem;">
             <img " src="${data.image}" class="card-img-top w-50 m-auto" alt="...">
-         <div class="card-body m-auto">
-            <h5 class="card-title">${data.phone_name}</h5>
-            <p class="card-text">Brand : ${data.brand} </p>
-            <a href="#" class="btn btn-primary" onclick="seeDetails('${data.slug}')">
-            See Details </a>
-         </div>
+            <div class="card-body m-auto">
+                <h5 class="card-title">${data.phone_name}</h5>
+                <p class="card-text">Brand : ${data.brand} </p>
+
+                <a href="#" class="btn btn-primary" onclick="seeDetails('${data.slug}')">
+                 See Details
+                </a>
+            </div>
         </div>
         `
         document.getElementById('display-card').appendChild(div)
 
     })
 
+
 }
+
 
 // see phone details 
 
@@ -68,8 +92,25 @@ const seeDetails = (id) => {
 }
 
 
+
+
+
+
+
 const displyDetailsData = (data) => {
     const othersInformation = data.others
+
+
+
+
+
+
+    // others property check 
+
+
+
+
+
 
     document.getElementById('product-photo').innerHTML = ''
     document.getElementById('phone-details-mainfeature').innerHTML = ''
@@ -84,6 +125,7 @@ const displyDetailsData = (data) => {
 
 
 
+
     // display mainFeature
     const mainFeature = data.mainFeatures
     const key = Object.keys(mainFeature)
@@ -92,6 +134,8 @@ const displyDetailsData = (data) => {
         <h3>${data.name}</h3>
         <p class="opacity-50"> <b>Relaese Date : </b>${data.releaseDate}</p>
         `
+
+
     key.forEach((key, index) => {
         const Detailsdiv = document.createElement('div')
         Detailsdiv.innerHTML = `<p class="m-0"><b>${key}</b> : ${mainFeature[key]}</p>`
@@ -102,14 +146,24 @@ const displyDetailsData = (data) => {
     // // disply others information 
 
 
-    if (data.hasOwnProperty('others') === true) { //check property
+    if (data.hasOwnProperty('others') === true) {
         const key_information = Object.keys(othersInformation)
+
         key_information.forEach((key, index) => {
             const div = document.createElement('div')
             div.innerHTML = `<p class="m-0"><b>${key}</b> : ${othersInformation[key]}</p>`
             document.getElementById('phone-details-others').appendChild(div)
 
         })
+    } else {
+        console.log('othrs property nai');
     }
+
+
+
+
+
+
+
 
 }
